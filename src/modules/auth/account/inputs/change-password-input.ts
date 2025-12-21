ef0ba6e56afee673c0d971f2c253cb12eccdaccd
Crivoi Daniel <crivoidan2004@gmail.com>
2025-12-21 19:51:37 +0200

    Account settings: email & password update

--- src/modules/auth/account/inputs/change-password-input.ts
+++ src/modules/auth/account/inputs/change-password-input.ts
@@ -2,24 +2,18 @@ import { Field, InputType } from "@nestjs/graphql";
 import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 @InputType()
-export class CreateUserInput{
-
-    @Field(() => String)
-    @IsString()
-    @IsNotEmpty()
-    @Matches(/^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/)
-    public username: string
-
-    @Field(() => String)
-    @IsString()
-    @IsNotEmpty()
-    @IsEmail()
-    public email: string
+export class ChangePasswordInput{
 
     @Field(() => String)
     @IsString()
     @IsNotEmpty()
     @MinLength(8)
-    public password: string
+    public oldpassword: string
+
+    @Field(() => String)
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    public newpassword: string
 
 }
